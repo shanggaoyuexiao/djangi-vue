d90f61b754c8f9522cce7ee7dbed4ee88532fa4b
sgyx <1175748902@qq.com>
2021-09-01 16:14:25 +0800

    pycharm可视化操作git

--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#ctl + k 来进行git add git commit操作
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
